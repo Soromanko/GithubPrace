11a3e782d62abf20043bf6ef17527c4f6af331e1
Soromanko <samuel.svach@orbiszlin.cz>
2025-09-16 11:00:09 +0200

    odcitani

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main() {
     int a;
     int b;
     int sum;
+    int odcitani;
 
     printf("First number: ");  //fix number 1
     scanf("%d", &a);
@@ -13,7 +14,10 @@ int main() {
 
     sum = a + b;
 
+    odcitani = a - b;
+
     printf("The sum is: %d\n", sum); //this is the sum
+    printf("The number is: %d\n", odcitani);
 
     return 0;
 }
