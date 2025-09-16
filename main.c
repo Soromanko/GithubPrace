99428ad544239041a8b04cedb2b68d288566c2cf
Soromanko <samuel.svach@orbiszlin.cz>
2025-09-16 10:48:30 +0200

    3rd commit

--- main.c
+++ main.c
@@ -3,12 +3,17 @@
 int main() {
     int a;
     int b;
+    int sum;
 
     printf("First number: ");
     scanf("%d", &a);
 
     printf("Second number: ");
     scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("The sum is: %d\n", sum);
 
     return 0;
 }
